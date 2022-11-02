8242f9c831446d24a204a08e69e2e1662c5c4d90
EngineeringDeveloper <ryan.c.parki@gmail.com>
2022-11-02 21:32:10 +0000

    Added Github pages config and setting for static svelte

---
+++ docs/_app/immutable/chunks/1-c918bc79.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4a9889e6.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-0f582923.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ac8a0b4f.js";export{t as component};
