2c645949d987404a94d1d38b51b38496dff3d6fd
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-01-16 21:34:43 +0000

    Addes some exampls - homepage first now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    // Examples https://awesome-portfolios.vercel.app/
 </script>
 
 <div class="container flex flex-col mx-auto w-full items-center">
-    <h1 class="text-3xl font-bold underline">This is Me</h1>
+    <h1 class="text-3xl font-bold underline">Ryan</h1>
+    <div>Mechanical Engineer</div>
+    <div>Programmer</div>
+    <div>Technologies</div>
     <footer class= "absolute bottom-1">Ryan</footer>
 </div>
 
