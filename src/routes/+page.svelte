50fed9a877909035b6644930dec045222647ad89
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-01-27 21:35:05 +0000

    Added Fancy block for styling on hover !IMPACT

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,26 @@
 </script>
 
 <div class="group show-all">
-    <span class="impact group-hover:opacity-10 hover:!opacity-100 peer "
-        >IM RYAN</span
+    <span class="impact group-hover:opacity-10 hover:!opacity-100 peer"
+        >I'M RYAN.</span
     >
-    <span class="impact info-block"
+    <span class="impact info-block hover:fancy-block"
         >MECHANICAL DESIGN ENGINEER,
         <div class="info">More info</div></span
     >
-    <span class="impact info-block">FULL STACK PROGRAMMER,</span>
-    <span class="impact info-block">DATA ANALYIST,</span>
-    <span class="impact info-block">& CYCLIST</span>
+    <span class="impact info-block hover:fancy-block">FULL STACK PROGRAMMER,</span>
+    <span class="impact info-block hover:fancy-block">DATA ANALYIST</span>
+    <span class="impact info-block hover:fancy-block">& CYCLIST</span>
 </div>
+
+<!-- make the commas and &s dissapear -->
 
 <style lang="postcss">
     .show-all {
         @apply container flex flex-col m-auto h-full w-full items-center;
     }
     .info-block {
-        @apply group-hover:opacity-10 hover:!opacity-100 peer-hover:!opacity-100;
+        @apply group-hover:opacity-10 hover:!opacity-100 peer-hover:!opacity-100 relative hover:m-10;
     }
 
     .info {
