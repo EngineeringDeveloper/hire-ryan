374babfbcd2058a0d47f13900fc162e1e32d500c
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-01-27 22:37:49 +0000

    Working implementation of pop out Info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,36 @@
 <script lang="ts">
     // Examples https://awesome-portfolios.vercel.app/
-
-    let showMechInfo = false;
-    let showStackInfo = false;
-    let showDataInfo = false;
 </script>
 
-<div class="group show-all">
+<blockquote class="group show-all">
     <span class="impact group-hover:opacity-10 hover:!opacity-100 peer"
-        >I'M RYAN.</span
-    >
-    <span
-        on:mouseover={() => (showMechInfo = true)}
-        on:focus={() => (showMechInfo = true)}
-        on:mouseleave={() => (showMechInfo = false)}
-        on:focusout={() => (showMechInfo = false)}
-        class="impact info-block hover:fancy-block"
-        >MECHANICAL DESIGN ENGINEER
-        <!-- {#if !showMechInfo}
-            ,
-        {/if} -->
+        >I'M RYAN.
     </span>
-    {#if showMechInfo}
-        <div class="info">More info</div>
-    {/if}
+    <span class="info-block group/mech">
+        <div class= "impact group-hover/mech:fancy-block">
+            MECHANICAL DESIGN ENGINEER
+        </div>
+        <div class="info group-hover/mech:!inline ">More info on Mechanical Engineering</div>
+    </span>
 
     <span class="impact info-block hover:fancy-block"
         >FULL STACK PROGRAMMER</span
     >
     <span class="impact info-block hover:fancy-block">DATA ANALYIST</span>
     <span class="impact info-block hover:fancy-block">CYCLIST</span>
-</div>
+</blockquote>
 
 <!-- make the commas and &s dissapear -->
 <style lang="postcss">
     .show-all {
-        @apply container flex flex-col m-auto h-full w-full items-center;
+        @apply container flex flex-col m-auto items-center;
     }
     .info-block {
-        @apply group-hover:opacity-10 hover:!opacity-100 peer-hover:!opacity-100 relative hover:m-10;
+        @apply group-hover:opacity-10 hover:!opacity-100 peer-hover:!opacity-100 hover:mb-20 transition-all duration-300;
     }
 
     .info {
-        /* @apply hidden hover:visible; */
+        @apply hidden transition-all duration-300;
     }
 
     .impact {
