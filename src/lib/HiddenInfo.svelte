eaae7418b3eaffa781b68e0470451d040e5d731a
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-01-30 19:22:06 +0000

    Basic implemtation of fit wrapping of div

--- src/lib/HiddenInfo.svelte
+++ src/lib/HiddenInfo.svelte
@@ -1,18 +1,59 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
+
     export let title: string;
     export let summary: string;
     export let tilt: boolean = false;
 
+    let sections : string[] = title.split(" ")
+    let breakNumber = 0
+    let breakTitle = false
+    $: brokenTitle = [sections.slice(0, sections.length  - breakNumber).join(" "), ...sections.slice(sections.length - breakNumber,  sections.length) ]
+    
+
     const topClass = `group flex flex-col w-fit group-hover/top:opacity-10 hover:!opacity-100 peer-hover:!opacity-100`;
-    let baseClass = tilt
-        ? "impact w-fit group-hover:clockwise group-hover:bg-pink-500 group-hover:text-black "
-        : "impact w-fit group-hover:anti-clockwise group-hover:bg-pink-500 group-hover:text-black ";
+    let titleClass = tilt
+        ? "impact w-fit group-hover:clockwise group-hover:bg-pink-500 group-hover:text-black whitespace-nowrap grid grid-row"
+        : "impact w-fit group-hover:anti-clockwise group-hover:bg-pink-500 group-hover:text-black whitespace-nowrap grid grid-row";
     const subClass = `h-0 group-hover:h-52 overflow-hidden transition-height duration-300`;
+
+    let titleDiv: HTMLDivElement;
+    const isEllipsisActive = () => {
+        console.log(titleDiv.offsetWidth > window.outerWidth, title);
+        if (titleDiv.offsetWidth > window.outerWidth) {
+            breakNumber += 1
+            breakTitle =  true
+            console.log(breakNumber, title)
+        } else {
+            breakNumber = 0
+            breakTitle =  false
+        } 
+        
+    };
+
+
+    onMount(() => {		
+		window.addEventListener('resize', isEllipsisActive);
+        isEllipsisActive()
+		
+		return () => {
+			window.removeEventListener('resize', isEllipsisActive);
+		}
+	});
+
 </script>
 
 <div class={topClass}>
-    <div class={baseClass}>
-        {title}
+    <div class={titleClass} bind:this={titleDiv}>
+        {#if breakTitle}
+        {#each brokenTitle as sect}
+            <div>{sect}</div>
+        {/each}                    
+        {:else}
+            {title}
+        {/if}
+        
     </div>
     <div class={subClass}>
         <br />
