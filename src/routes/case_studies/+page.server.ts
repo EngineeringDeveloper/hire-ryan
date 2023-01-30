d0dc76f289044c778f74bd1843cef3ddea152b03
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-01-30 21:08:40 +0000

    Type updates for page load function for case studies

--- src/routes/case_studies/+page.server.ts
+++ src/routes/case_studies/+page.server.ts
@@ -1,28 +1,41 @@
 import type { PageServerLoad } from "./$types";
-import { base } from '$app/paths'
+import { base } from "$app/paths";
+
+interface meta {
+    title: string;
+    date: string;
+    tags: string[];
+    image: string;
+}
+
+type caseStudyLoad = [
+    string,
+    () => Promise<{ metadata: meta }>
+][];
 
 const fetchMarkdownPosts = async () => {
-    const allPostFiles = import.meta.glob('./**.md')
-    const iterablePostFiles = Object.entries(allPostFiles)
-    
-    const allPosts = await Promise.all(
-      iterablePostFiles.map(async ([path, resolver]) => {
-        const { metadata } = await resolver()
-        const postPath = `${base}/case_studies/${path.replace("./", "").replace(".md", "")}`
-  
-        return {
-          meta: metadata,
-          path: postPath,
-        }
-      })
-    )
-  
-    return allPosts
-  }
+    const allPostFiles: any = import.meta.glob("./**.md");
+    const iterablePostFiles: caseStudyLoad = Object.entries(allPostFiles);
 
+    const allPosts = await Promise.all(
+        iterablePostFiles.map(async ([path, resolver]) => {
+            const { metadata } = await resolver();
+            const postPath = `${base}/case_studies/${path
+                .replace("./", "")
+                .replace(".md", "")}`;
+
+            return {
+                meta: metadata,
+                path: postPath,
+            };
+        })
+    );
+
+    return allPosts;
+};
 
 export const load = (async () => {
-    const posts = await fetchMarkdownPosts()
-    console.log(posts)
-    return {posts};
+    const posts = await fetchMarkdownPosts();
+    console.log(posts);
+    return { posts };
 }) satisfies PageServerLoad;
