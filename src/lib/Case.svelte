91891f6e860a2686142a0c04020e5cfe07242b29
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-01 19:04:13 +0000

    Added Marquee for tags need to fix overflow

--- src/lib/Case.svelte
+++ src/lib/Case.svelte
@@ -5,34 +5,42 @@
 </script>
 
 <a
-                    class="underline p-1 w-32 h-32 group hover:w-52 hover:text-2xl ease-in-out duration-300"
-                    href={caseStudy.path}
-                >
-                    <div class="absolute w-32 group-hover:w-52">
-                        <h2 class="text-orange-500">
-                            {caseStudy.meta.title}
-                        </h2>
-                        <div
-                            class="opacity-0 group-hover:opacity-80 hover:transition-opacity flex flex-row flex-wrap"
-                        >
-                            {#each caseStudy.meta.tags as tag}
-                                <h2 class="px-1">#{tag}</h2>
-                            {/each}
-                        </div>
-                    </div>
+    class="underline p-1 w-32 h-32 group hover:w-52 hover:text-2xl ease-in-out duration-300"
+    href={caseStudy.path}
+>
+    <div class="absolute group-hover:w-52">
+        <h2 class="text-orange-500 w-32">
+            {caseStudy.meta.title}
+        </h2>
+        <div class= "relative overflow-x-hidden w-32">
+            <div
+                class="whitespace-nowrap animate-marquee opacity-0 group-hover:opacity-80 hover:transition-opacity flex flex-row flex-wrap"
+            >
+                {#each caseStudy.meta.tags as tag}
+                    <h2 class="px-1">#{tag}</h2>
+                {/each}
+            </div>
+            <div
+                class="absolute top-0 whitespace-nowrap animate-marquee2 opacity-0 group-hover:opacity-80 hover:transition-opacity flex flex-row flex-wrap"
+            >
+                {#each caseStudy.meta.tags as tag}
+                    <h2 class="px-1">#{tag}</h2>
+                {/each}
+            </div>
+        </div>
+    </div>
 
-                    {#if caseStudy.meta.image}
-                        <img
-                            class="object-none w-32 h-32 group-hover:w-52 ease-in-out duration-300"
-                            src={`/images/${caseStudy.meta.image}`}
-                            alt=""
-                        />
-                    {:else}
-                        <img
-                            class="object-none w-32 h-32 group-hover:w-52 ease-in-out duration-300"
-                            src={`/images/placeholder.png`}
-                            alt=""
-                        />
-                    {/if}
-                </a>
-
+    {#if caseStudy.meta.image}
+        <img
+            class="object-none w-32 h-32 group-hover:w-52 ease-in-out duration-300"
+            src={`/images/${caseStudy.meta.image}`}
+            alt=""
+        />
+    {:else}
+        <img
+            class="object-none w-32 h-32 group-hover:w-52 ease-in-out duration-300"
+            src={`/images/placeholder.png`}
+            alt=""
+        />
+    {/if}
+</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,20 @@ module.exports = {
     extend: {
       transitionProperty: {
         height: 'height',
+      },
+      animation: {
+        marquee: 'marquee 5s linear infinite',
+        marquee2: 'marquee2 5s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       }
     },
   },
