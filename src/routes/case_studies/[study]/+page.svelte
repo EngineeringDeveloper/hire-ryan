a85d228e5af391c4b51bcf49f5ded8d3f12887b9
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-02-04 15:54:38 +0000

    Added small home icon to Case Studies

--- src/routes/case_studies/[study]/+page.svelte
+++ src/routes/case_studies/[study]/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
     import type { PageData } from "./$types";
+    import { IconHome } from "@tabler/icons-svelte";
 
     export let data: PageData;
 </script>
 
 <svelte:head>
-  <title>Case Study - {data.meta.title}</title>
-  <meta property="og:title" content={data.meta.title} />
+    <title>Case Study - {data.meta.title}</title>
+    <meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
 <article>
-    <h1>{ data.meta.title }</h1>
+    <a href="/" class="absolute top-1 right-1"><IconHome/></a>
+    <h1>{data.meta.title}</h1>
     <p>Published: {data.meta.date}</p>
     <svelte:component this={data.content} />
 </article>
